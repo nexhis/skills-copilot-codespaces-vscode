5ae6325e03a4390fe150dd3f97b16c92cac72193
Nexhi Sula <113061281+nexhis@users.noreply.github.com>
2023-08-04 18:48:34 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -7,3 +7,12 @@ function skillsMember() {
     return member;
 }
 
+function skillsMember()
+{
+    var member = {
+        name: 'John Doe',
+        age: 30,
+        address: '123 Main St'
+    };
+    return member;
+}
